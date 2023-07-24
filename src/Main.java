d27941da21f4ec76ea4af654e4b8e91dda260b39
Anastasia <hagnum@mail.ru>
2023-07-24 13:06:33 +0500

    new file

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        var server = new Server(64);
+        server.addHandler("GET", "/messages", ((request, responseStream) ->
+                server.responseError(responseStream)));
+        server.addHandler("POST", "/messages", (request, responseStream) ->
+                server.responseError(responseStream));
+        server.addHandler("GET", "/classic.html", (request, outputStream) ->
+                server.defaultLinkConnection(outputStream, "/classic.html"));
+        server.addHandler("GET", "/spring.svg", (request, outputStream) ->
+                server.defaultLinkConnection(outputStream, "/spring.svg"));
+
+        server.startServer();
+    }
+}
